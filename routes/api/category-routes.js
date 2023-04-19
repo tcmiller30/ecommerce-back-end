a71fdcef6376b6d444e1a80323a92860945895ef
Travis <traviscmiller01@gmail.com>
2023-04-19 13:14:09 -0700

    edited category routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,6 +5,9 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll().then((categoryData) => {
+    res.json(categoryData);
+  });
   // be sure to include its associated Products
 });
 
